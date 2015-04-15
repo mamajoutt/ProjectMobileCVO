9f495133cf74c12ef24980706790ebcc3eea689e
LanceDH <kiep_out@hotmail.com>
2015-04-15 22:55:48 +0200

    Code Moodle Web Services added

---
+++ Code/Moodle-Webservice-Test/PROJECT_SCRATCHPAD/CalendarEvent.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace PROJECT_SCRATCHPAD
+{
+    class CalendarEvent
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Format { get; set; }
+        public string CourseId { get; set; }
+        public string GroupId { get; set; }
+        public string RepeatId { get; set; }
+        public string Modulename { get; set; }
+        public string Instance { get; set; }
+        public string Eventtype { get; set; }
+        public DateTime TimeStart { get; set; }
+        public string TimeDuration { get; set; }
+        public string Visible { get; set; }
+        public string Uuid { get; set; }
+        public string Sequence { get; set; }
+        public string TimeModified { get; set; }
+        public string SubscriptionId { get; set; }
+
+        public CalendarEvent(JObject jObject)
+        {
+            this.Id = (string)jObject["id"];
+            this.Name = (string)jObject["name"];
+            this.Description = (string)jObject["description"];
+            this.Format = (string)jObject["format"];
+            this.CourseId = (string)jObject["courseid"];
+            this.GroupId = (string)jObject["groupid"];
+            this.RepeatId = (string)jObject["repeatid"];
+            this.Modulename = (string)jObject["modulename"];
+            this.Instance = (string)jObject["instance"];
+            this.Eventtype = (string)jObject["eventtype"];
+            string unixStamp = (string)jObject["timestart"];
+            this.TimeStart = new DateTime(1970, 1, 1, 0, 0, 0, 0).AddSeconds(Convert.ToDouble(unixStamp));
+            this.TimeDuration = (string)jObject["timeduration"];
+            this.Visible = (string)jObject["visible"];
+            this.Uuid = (string)jObject["uuid"];
+            this.Sequence = (string)jObject["sequence"];
+            this.TimeModified = (string)jObject["timemodified"];
+            this.SubscriptionId = (string)jObject["subscriptionid"];
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}: {1} \r\n Info:  {2} \r\n  Deadline: {3} {4}", Id, Name, Description, TimeStart.ToLongDateString(), TimeStart.ToShortTimeString());
+        }
+    }
+}
---
+++ Code/Moodle-Webservice-Test/PROJECT_SCRATCHPAD/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using Json;
+using Newtonsoft.Json.Linq;
+using Moodle;
+
+namespace PROJECT_SCRATCHPAD
+{
+
+   
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string url = "moodle-cvomobile.rhcloud.com";
+
+            // Get token
+            string token = Moodle.Model.RequestTokenForService(url, "cvomobile", "Boerderijm1n#s", "mobile");
+
+            Console.WriteLine("Token: " + token);
+
+            // Get user Id using email
+                Console.Write("email: ");
+                string email = Console.ReadLine();
+
+                int userid = Moodle.Model.GetUserIdByEmail(url, token, email);
+                Console.WriteLine("userid:" + userid);
+
+            // get Courses user is enrolled in
+
+                List<Moodle.BLL.Course> enrolledCourses = Moodle.DAL.Course.GetUserEnrolledCourses(url, token, userid);
+
+                foreach (Moodle.BLL.Course c in enrolledCourses)
+                {
+                    Console.WriteLine(c.Id + ": " + c.FullName + " (" + c.ShortName + ")");
+
+                    foreach (Moodle.BLL.Assignment ass in Model.AssingmentSelectAll(url, token, c.Id))
+                    {
+                        //CalendarEvent c = new CalendarEvent(assignment);
+                        Console.WriteLine(ass.Id + ": " + ass.Name);
+
+                        //all scores
+                        /*foreach (Moodle.BLL.Grade grade in Model.GradeSelectAll(url, token, ass.Id))
+                        {
+                            Console.WriteLine(grade.UserId + ": " + grade.Score);
+                        }*/
+
+                        //only scores of user
+                        string userGrade = Model.GradeSelectOne(url, token, ass.Id, userid);
+
+                        Console.WriteLine("Your score: " + (!userGrade.Equals("-1") ? userGrade : "No score yet"));
+                        Console.WriteLine();
+                    }
+
+                }
+            Console.Read();
+        }
+    }
+}
