9021639988e7608fa0787bebf967e76942ddd894
mamajoutt <mo26602011@hotmail.com>
2015-04-27 01:59:56 +0200

    DAL & BLL gerevert
    ik had problemen met te commiten hierdoor is alles kwijt gegaan en ben
    ik terug van vorig punt begonnen

--- Code/Test/CVO-Mobile/CVO-Mobile/App_Code/Administratix/DAL.cs
+++ Code/Test/CVO-Mobile/CVO-Mobile/App_Code/Administratix/DAL.cs
@@ -3,9 +3,141 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+namespace MobileCVO.DAL
+{
+    interface IDal<TypeEntity>
+    {
+        string message {get;} 
+        //bool Update(TypeEntity entity); 
+        //int Insert(TypeEntity entity); 
+        //bool Delete(int Id); 
+        TypeEntity SelectOne(int id);
+        List<TypeEntity> SelectAll();
+    }
+}
+
 namespace Administratix.DAL
 {
-    public class DAL
+    //DAL voor Cursist 
+    public class Cursist : MobileCVO.DAL.IDal<Administratix.BLL.Cursist>
     {
+        public Cursist()
+        {
+            // 
+        }
+
+        public Administratix.BLL.Cursist SelectOne(int id)
+        {
+            //ProjectMobileCVO.BLL.Cursist cursist = new BLL.Cursist(); 
+
+        }
+    }
+
+    //DAL voor Personeel 
+    public class Personeel : MobileCVO.DAL.IDal<Administratix.BLL.Personeel>
+    {
+        public Personeel()
+        {
+            //this.message = ""; 
+        }
+    }
+
+    //DAL voor Schooljaar 
+    public class Schooljaar : MobileCVO.DAL.IDal<Administratix.BLL.Schooljaar>
+    {
+        public Schooljaar()
+        {
+            //this.message = ""; 
+        }
+    }
+
+    //DAL voor Kalender 
+    public class Kalender : MobileCVO.DAL.IDal<Administratix.BLL.Kalender>
+    {
+        public Kalender()
+        {
+            //this.message = ""; 
+        }
+    }
+
+    //DAL voor Evenement 
+    public class Evenement : MobileCVO.DAL.IDal<Administratix.BLL.Evenement>
+    {
+        public Evenement()
+        {
+            //this.message = ""; 
+        }
+    }
+
+    //DAL voor Module 
+    public class Module : MobileCVO.DAL.IDal<Administratix.BLL.Module>
+    {
+        public Module()
+        {
+            //this.message = ""; 
+        }
+    }
+
+    //DAL voor IngerichteModulevariant 
+    public class IngerichteModulevariant : MobileCVO.DAL.IDal<Administratix.BLL.IngerichteModulevariant>
+    {
+        public IngerichteModulevariant()
+        {
+            //this.message = ""; 
+        }
+    }
+
+    //DAL voor Plaatsing 
+    public class Plaatsing : MobileCVO.DAL.IDal<Administratix.BLL.Plaatsing>
+    {
+        public Plaatsing()
+        {
+            //this.message = ""; 
+        }
+    }
+
+    //DAL voor Les 
+    public class LesDavinci : MobileCVO.DAL.IDal<Administratix.BLL.LesDavinci>
+    {
+        public LesDavinci()
+        {
+            //this.message = ""; 
+        }
+    }
+
+    //DAL voor Lokaal 
+    public class Lokaal : MobileCVO.DAL.IDal<Administratix.BLL.Lokaal>
+    {
+        public Lokaal()
+        {
+            //this.message = ""; 
+        }
+    }
+
+    //DAL voor Resultaat 
+    public class PlaatsingResultaat : MobileCVO.DAL.IDal<Administratix.BLL.PlaatsingResultaat>
+    {
+        public PlaatsingResultaat()
+        {
+            //this.message = ""; 
+        }
+    }
+
+    //DAL voor Attest 
+    public class Attest : MobileCVO.DAL.IDal<Administratix.BLL.Attest>
+    {
+        public Attest()
+        {
+            //this.message = ""; 
+        }
+    }
+
+    //DAL voor TweedeZit 
+    public class TweedeZit : MobileCVO.DAL.IDal<Administratix.BLL.TweedeZit>
+    {
+        public TweedeZit()
+        {
+            //this.message = ""; 
+        }
     }
 }
--- Code/Test/CVO-Mobile/CVO-Mobile/App_Code/Moodle/BLL.cs
+++ Code/Test/CVO-Mobile/CVO-Mobile/App_Code/Moodle/BLL.cs
@@ -7,104 +7,35 @@ using Newtonsoft.Json.Linq;
 
 namespace Moodle.BLL
 {
-    public class Grade
-    {
-        public int Id { get; set; }
-        public int UserId { get; set; }
-        public int AttemptNumber { get; set; }
-        public DateTime TimeModified { get; set; }
-        public int Grader { get; set; }
-        public string Score { get; set; }
-
-        public Grade(JObject jSon)
-        {
-            this.Id = Convert.ToInt32((string)jSon["id"]);
-            this.UserId = Convert.ToInt32((string)jSon["userid"]);
-            this.AttemptNumber = Convert.ToInt32((string)jSon["attemptnumber"]);
-            int unixTimeModified = Convert.ToInt32((string)jSon["timemodified"]);
-            this.TimeModified = new DateTime(1970, 1, 1, 0, 0, 0, 0).AddSeconds(unixTimeModified);
-            this.Grader = Convert.ToInt32((string)jSon["grader"]);
-            this.Score = (string)jSon["grade"];
-        }
-    }
 
     public class Assignment
     {
-        public int Id { get; set; }    //assignment id
-        public int Course { get; set; }    //course id
-		public string Name { get; set; }  //assignment name
-        public DateTime DueDate { get; set; }    //assignment due date
-		public int AllowSubmissionsFromDate { get; set; }    //allow submissions from date
-		public int Grade { get; set; }    //grade type
-		public int TimeModified { get; set; }    //last time assignment was modified
-		public DateTime CutOffDate { get; set; }   //date after which submission is not accepted without an extension
-		public int MaxAttempts { get; set; }    //maximum number of attempts allowed
+        public int Id { get; set; }
+        public int CourseId { get; set; }  
+		public string Name { get; set; }  
+        public DateTime DueDate { get; set; }
+        public DateTime AllowSubmissionsFromDate { get; set; }
+		public int MaxGrade { get; set; }
+        public DateTime TimeModified { get; set; }
+		public DateTime CutOffDate { get; set; }
+		public int MaxAttempts { get; set; }
         public string UserScore { get; set; } 
-        public List<Moodle.BLL.Grade> Grades { get; set; } 
 
         public Assignment(JObject jSon)
         {
             this.Id = Convert.ToInt32(jSon["id"]);
-            this.Course = Convert.ToInt32(jSon["course"]);
+            this.CourseId = Convert.ToInt32(jSon["course"]);
             this.Name = (string)jSon["name"];
             int unixTimeDueDate = Convert.ToInt32((string)jSon["duedate"]);
             this.DueDate = new DateTime(1970, 1, 1, 0, 0, 0, 0).AddSeconds(unixTimeDueDate);
-            //this.DueDate = Convert.ToInt32( jSon["duedate"]);
-            this.AllowSubmissionsFromDate = Convert.ToInt32( (string)jSon["allowsubmissionsfromdate"]);
-            this.Grade = Convert.ToInt32( (string)jSon["grade"]);
-            this.TimeModified = Convert.ToInt32( (string)jSon["timemodified"]);
+            int unixTimeAllowSubmissionsFromDate = Convert.ToInt32((string)jSon["duedate"]);
+            this.AllowSubmissionsFromDate = new DateTime(1970, 1, 1, 0, 0, 0, 0).AddSeconds(unixTimeAllowSubmissionsFromDate);
+            this.MaxGrade = Convert.ToInt32((string)jSon["grade"]);
+            int unixTimeTimeModified = Convert.ToInt32((string)jSon["duedate"]);
+            this.TimeModified = new DateTime(1970, 1, 1, 0, 0, 0, 0).AddSeconds(unixTimeTimeModified);
             int unixTimeCutOffDate = Convert.ToInt32((string)jSon["cutoffdate"]);
             this.CutOffDate = new DateTime(1970, 1, 1, 0, 0, 0, 0).AddSeconds(unixTimeCutOffDate);
-            //this.CutOffDate = Convert.ToInt32( (string)jSon["cutoffdate"]);
             this.MaxAttempts = Convert.ToInt32( (string)jSon["maxattempts"]);
-        }
-    }
-
-    public class CalendarEvent
-    {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Format { get; set; }
-        public string CourseId { get; set; }
-        public string GroupId { get; set; }
-        public string RepeatId { get; set; }
-        public string Modulename { get; set; }
-        public string Instance { get; set; }
-        public string Eventtype { get; set; }
-        public DateTime TimeStart { get; set; }
-        public string TimeDuration { get; set; }
-        public string Visible { get; set; }
-        public string Uuid { get; set; }
-        public string Sequence { get; set; }
-        public string TimeModified { get; set; }
-        public string SubscriptionId { get; set; }
-
-        public CalendarEvent(JObject jObject)
-        {
-            this.Id = (string)jObject["id"];
-            this.Name = (string)jObject["name"];
-            this.Description = (string)jObject["description"];
-            this.Format = (string)jObject["format"];
-            this.CourseId = (string)jObject["courseid"];
-            this.GroupId = (string)jObject["groupid"];
-            this.RepeatId = (string)jObject["repeatid"];
-            this.Modulename = (string)jObject["modulename"];
-            this.Instance = (string)jObject["instance"];
-            this.Eventtype = (string)jObject["eventtype"];
-            string unixStamp = (string)jObject["timestart"];
-            this.TimeStart = new DateTime(1970, 1, 1, 0, 0, 0, 0).AddSeconds(Convert.ToDouble(unixStamp));
-            this.TimeDuration = (string)jObject["timeduration"];
-            this.Visible = (string)jObject["visible"];
-            this.Uuid = (string)jObject["uuid"];
-            this.Sequence = (string)jObject["sequence"];
-            this.TimeModified = (string)jObject["timemodified"];
-            this.SubscriptionId = (string)jObject["subscriptionid"];
-        }
-
-        public override string ToString()
-        {
-            return string.Format("{0}: {1} \r\n Info:  {2} \r\n  Deadline: {3} {4}", Id, Name, Description, TimeStart.ToLongDateString(), TimeStart.ToShortTimeString());
         }
     }
 
