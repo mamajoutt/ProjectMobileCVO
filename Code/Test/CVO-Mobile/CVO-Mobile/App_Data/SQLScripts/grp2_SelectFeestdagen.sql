29d9dbdde8abd517e97f0a361e427b813470c451
mamajoutt <mo26602011@hotmail.com>
2015-05-18 11:26:38 +0200

    SelectFeestdagen van Jo teruggezet en v2 van mo

--- Code/Test/CVO-Mobile/CVO-Mobile/App_Data/SQLScripts/grp2_SelectFeestdagen.sql
+++ Code/Test/CVO-Mobile/CVO-Mobile/App_Data/SQLScripts/grp2_SelectFeestdagen.sql
@@ -1,27 +1,32 @@
---Mohamed Amajooutt
---17/05/2015
+﻿--BP/JR
 
---Lijst geven van alle feestdagen vanaf huidige datum gesorteerd op datum
-use Administratix_cursist
-go
-
-if exists (select 1 from sysobjects where name = 'grp2_SelectFeestdagen' AND type = 'P') -- P = procedure
-
-begin
-	drop proc grp2_SelectFeestdagen
-end
-go
-
-create procedure grp2_SelectFeestdagen
-as
-begin
+use Administratix_cursist  
+go 
+  
+if exists (select 1 from sysobjects where name = 'grp2_SelectFeestdagen' AND type = 'P') -- P = procedure 
+  
+begin 
+drop proc grp2_SelectFeestdagen 
+end 
+go 
+  
+create procedure grp2_SelectFeestdagen 
+( 
+@Date1 datetime, 
+@Date2 datetime 
+ 
+) 
+  
+as 
+  
+begin 
+  
 select 
-	Kalender.Id
-	,Schooljaar.Omschrijving
-	,Convert (nvarchar, Kalender.Datum, 5) as Datum
-	,Kalender.Omschrijving
-	from Kalender
-	inner join Schooljaar on Kalender.IdSchooljaar = Schooljaar.Id
-	where Kalender.Datum >=GETDATE() 
-	order by Kalender.Datum 
-end
+ Id, 
+ Convert (nvarchar, Kalender.Datum, 5) as Datum,  
+ IdSchooljaar,  
+ Omschrijving,  
+ IdVerlofdagType 
+ --from Kalender where Kalender.Datum >= GETDATE() 
+ from Kalender where Kalender.Datum <= @Date2 and Kalender.Datum >= @Date1 
+ end
---
+++ Code/Test/CVO-Mobile/CVO-Mobile/App_Data/SQLScripts/grp2_SelectFeestdagenVersieMo.sql
@@ -0,0 +1,27 @@
+--Mohamed Amajooutt
+--17/05/2015
+
+--Lijst geven van alle feestdagen vanaf huidige datum gesorteerd op datum
+use Administratix_cursist
+go
+
+if exists (select 1 from sysobjects where name = 'grp2_SelectFeestdagen' AND type = 'P') -- P = procedure
+
+begin
+	drop proc grp2_SelectFeestdagen
+end
+go
+
+create procedure grp2_SelectFeestdagenVersieMo
+as
+begin
+select 
+	Kalender.Id
+	,Schooljaar.Omschrijving
+	,Convert (nvarchar, Kalender.Datum, 5) as Datum
+	,Kalender.Omschrijving
+	from Kalender
+	inner join Schooljaar on Kalender.IdSchooljaar = Schooljaar.Id
+	where Kalender.Datum >=GETDATE() 
+	order by Kalender.Datum 
+end
